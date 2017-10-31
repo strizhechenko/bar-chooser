3f54a2cd86ecab9523a8c84befbc958ece876ddc
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-10-31 17:41:05 +0500

    Added(setup): README to show in PYPI

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 # coding=utf-8
+import os
 from setuptools import setup
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
 
 setup(
     name='bar-chooser',
-    version='0.1.0',
+    version='0.1.1',
     packages=['bar_chooser'],
     url='https://github.com/strizhechenko/bar-chooser',
     license='MIT',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     description='Script that help you to choose a bar to drink with someone.',
+    long_description=(read('README.rst')),
     entry_points={
         'console_scripts': [
             'bar-chooser=bar_chooser.__init__:main',
